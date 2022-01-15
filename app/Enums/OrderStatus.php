7adcb552ac9e229d4ee19ee5c761df227d78688d
Mahe Karim <MaheKarim@deligram.com>
2022-01-16 02:34:35 +0600

    Enums Introduced

---
+++ app/Enums/OrderStatus.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Enums;
+
+interface OrderStatus
+{
+    const APPROVE = 1;
+    const REJECT = 2;
+    const PENDING = 3;
+}
---
+++ resources/lang/en/order.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    "status" => [
+        \App\Enums\OrderStatus::APPROVE => "Approve",
+        \App\Enums\OrderStatus::REJECT => "Reject",
+        \App\Enums\OrderStatus::PENDING => "Approve",
+    ],
+];
