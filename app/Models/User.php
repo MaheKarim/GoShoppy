2ded870bb82cff7700f1baa458b6e96d0b3f9ee0
Mahe Karim <MaheKarim@deligram.com>
2022-06-26 02:43:50 +0600

    Remove Unused Relationship :coffin:

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,10 +47,6 @@ class User extends Authenticatable implements HasMedia
     {
         return $this->belongsTo(Role::class);
     }
-    public function shipOrders()
-    {
-        return $this->hasMany(ShipForMe::class);
-    }
 
     public function hasPermission($permission):bool {
         return $this->role->permissions()->where('slug', $permission)->first() ? true : false;
